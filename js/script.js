875970a90ce723640b2fe6f14e17361c76be58ef
Aanandita <aanandita.pujji@gmail.com>
2025-07-14 21:53:36 -0400

    added css for gradient, added basic js functionality, added images for buttons, replaced prompt with a settings dropdown

--- js/script.js
+++ js/script.js
@@ -1,7 +1,58 @@
-const container = document.querySelector("#container");
-console.log(container);
-for (let i = 0; i < 16; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+const clearGrid = () => {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.classList.remove("hover");
+    });
 }
+
+const clearSquare = (square) => {
+    square.classList.remove("hover");
+}
+
+const addSquare = (square) => {
+    square.classList.add("hover");
+}
+
+const buildGrid = (num = 32) => {
+    console.log(num);
+    const container = document.querySelector("#container");
+    container.replaceChildren();
+    const gridSize = num * num;
+    for (let i = 0; i < gridSize; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        const flexBasis = Math.round((600 / num) * 100) / 100;
+        const height = Math.round((600 / num) * 100) / 100;
+
+        square.style.flexBasis = `${flexBasis.toString()}px`;
+        square.style.height = `${height.toString()}px`;
+
+        square.addEventListener("mouseenter", (ev) => addSquare(ev.target));
+        square.addEventListener("click", (ev) => clearSquare(ev.target));
+
+        container.appendChild(square);
+    }
+};
+
+const getGridSize = () => {
+    let numberOfSquares = prompt("Enter number of squares per row:");  
+    const MAX_SQUARES = 100;
+
+    if (numberOfSquares > MAX_SQUARES) {
+        alert("Enter a number less than or equal to 100");
+    } else {
+        buildGrid(numberOfSquares);
+    }
+};
+
+const showSettings = () => {
+    const settings = document.querySelector("#settings");
+    settings.classList.toggle("show");
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    showSettings();
+});
+
+buildGrid();
