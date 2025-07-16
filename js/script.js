f332159d4027288e849a1fb59a604380dee2cd2d
Aanandita <aanandita.pujji@gmail.com>
2025-07-16 15:15:39 -0400

    more cleanup

--- js/script.js
+++ js/script.js
@@ -24,13 +24,27 @@ const toggleSettingsModal = () => {
     settings.classList.toggle("show");
 };
 
+const saveSettings = (currentGridSize) => {
+    const sizeInput = document.querySelector("#grid-size-input");
+    let newGridSize = parseInt(sizeInput.value, 10);
+    if (newGridSize !== currentGridSize && Number(newGridSize) && newGridSize > 0) {
+        newGridSize = newGridSize <= 100 ? newGridSize : 100;
+        buildGrid(newGridSize);
+        currentGridSize = newGridSize;
+    }
+
+    toggleSettingsModal();
+}
+
 const init = () => {
     let currentGridSize = 32;
     const MAX_SQUARES = 100;
     const saveButton = document.querySelector("#save-btn");
 
     let isMouseDown = false;
-    document.addEventListener("mousedown", () => isMouseDown = true );
+    document.addEventListener("mousedown", (ev) => { 
+        if (ev.button === 0) isMouseDown = true;
+    });
     document.addEventListener("mouseup", () => isMouseDown = false );
 
     const header = document.querySelector("#header");
@@ -40,20 +54,13 @@ const init = () => {
         else if (id === "clear-btn") clearGrid();
     });
 
-    saveButton.addEventListener("click", () => {
-        const input = document.querySelector("#grid-size-input").value;
-        let newGridSize = parseInt(input, 10);
-        if (newGridSize !== currentGridSize && Number(newGridSize) && newGridSize > 0) {
-            newGridSize = newGridSize <= 100 ? newGridSize : 100;
-            buildGrid(newGridSize);
-            currentGridSize = newGridSize;
-        }
-        toggleSettingsModal();
-    });
+    saveButton.addEventListener("click", () => saveSettings(currentGridSize));
 
     buildGrid(currentGridSize);
 
-    container.addEventListener("mousedown", (ev) => ev.target.classList.toggle("fill"));
+    container.addEventListener("mousedown", (ev) => {
+        if (ev.button === 0) ev.target.classList.toggle("fill")
+    });
     container.addEventListener("mouseover", (ev) => {
         if (isMouseDown) ev.target.classList.add("fill");
     });
